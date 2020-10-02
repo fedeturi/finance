70c45f530ae627f0f3d9ec12504412d762a7dcbd
Fede Brun GDP <fedejbrun@gmail.com>
2020-10-02 17:51:52 -0300

    market_data from ROFEX reMarkets environment. Trade History and full Book.

--- dma-rofex/rofexclient.py
+++ dma-rofex/rofexclient.py
@@ -1,59 +1,183 @@
-import socket
-import ssl
 import logging
+import sys
+import traceback
 from os import system, name
+import os
 import time
+from pprint import pprint
+import pandas as pd
+
 import pyRofex
 
+width = os.get_terminal_size().columns
+dash_line = "-" * width
 
-class ConnectionROFEX:
-    def __init__(self, verbose=0):
-        # Set the the parameter for the REMARKET environment
-        pyRofex.initialize(user="fedejbrun5018",
-                           password="ugklxY0*",
-                           account="REM5018",
-                           environment=pyRofex.Environment.REMARKET)
 
-    # First we define the handlers that will process the messages and exceptions.
-    def market_data_handler(message):
-        print("Market Data Message Received: {0}".format(message))
+class ROFEXClient:
+    """Start Client to connect to ROFEX DMA server
 
-    def order_report_handler(message):
-        print("Order Report Message Received: {0}".format(message))
+    :param user: User.
+    :type user: Str.
+    :param password: User Password.
+    :type password: Str.
+    :param account: Account provided by Market Authority.
+    :type account: Str.
+    :param environment: Market environment. (demo = reMarkets; live = ROFEX)
+    :type environment: Str.
+    """
+    def __init__(self, user, password, account, environment, verbose=3):
 
-    def error_handler(message):
-        print("Error Message Received: {0}".format(message))
+        self.subscribed_instruments = []
 
-    def exception_handler(e):
-        print("Exception Occurred: {0}".format(e.message))
+        # Define environment
+        env_param = environment.lower()
+        environments = {
+            'demo': pyRofex.Environment.REMARKET,
+            'live': pyRofex.Environment.LIVE,
+        }
+        env = environments.get(env_param)
 
-    def connect(self):
-        # Initiate Websocket Connection
+        # Create logging file
+        filename = 'c:/logs/ROFEX' + env_param \
+                   + time.strftime('%Y-%m-%d_%H%M%S', time.localtime(time.time())) + '.log'
+        logging.basicConfig(level=logging.DEBUG, format='%(asctime)s-%(threadName)s-%(message)s', filename=filename)
+
+        # TODO No entiendo muy bien cual es la funcion de los AddLevelName
+
+        verbosity = verbose
+        if verbosity == '1':
+            logging.addLevelName('DEBUG', 10)
+        elif verbosity == '2':
+            logging.addLevelName('DEBUG', 20)
+        elif verbosity == '3':
+            logging.addLevelName('DEBUG', 30)
+        else:
+            logging.basicConfig(level=None, format='%(asctime)s-%(threadName)s-%(message)s', filename=filename)
+            logging.addLevelName('DEBUG', 30)
 
         try:
+            self.connect(user, password, account, env)
+        except KeyboardInterrupt:
+            self.disconnect()
+
+    # ==================================================================================================================
+    #   Start HANDLERS definition
+    # ==================================================================================================================
+    def market_data_handler(self, message):
+        try:
+            self.process_market_data_message(message)
+        except Exception:
+            traceback.print_exec()
+
+    def order_report_handler(self, message):
+        print("Order Report Message Received: {0}".format(message))
+
+    def error_handler(self, message):
+        print("Error Message Received: {0}".format(message))
+
+    def exception_handler(self, e):
+        print("Exception Occurred: {0}".format(e.message))
+
+    # ==================================================================================================================
+    #   End HANDLERS definition
+    # ==================================================================================================================
+
+    # ==================================================================================================================
+    #   Start UTIL FUNCTIONS definition
+    # ==================================================================================================================
+
+    def connect(self, user, password, account, environment, ):
+
+        clear_screen()
+        print(dash_line)
+        message = f'Connecting to {environment} DMA server'
+        print(message.center(width))
+        print(dash_line)
+
+        if logging.getLevelName('DEBUG') > 1:
+            logging.debug(f'ROFEXClient: Starting connection to {environment} DMA Server ')
+        try:
+            # Initialize Environment
+            pyRofex.initialize(user=user,
+                               password=password,
+                               account=account,
+                               environment=environment)
+
+            # Initialize WebSocket Cnnection with Handlers
+
             pyRofex.init_websocket_connection(market_data_handler=self.market_data_handler,
-                                              order_report_handler=self.order_report_handler,
                                               error_handler=self.error_handler,
                                               exception_handler=self.exception_handler)
-        except Exception as ex:
-            pass
+        except Exception as e:
+            if logging.getLevelName('DEBUG') > 1:
+                logging.debug(f'ROFEXClient ERROR: En exception occurred {e}')
 
-    def subscribe_instruments(self, firs_instrument, **args_instruments):
+            error_msg = "\033[0;30;47mERROR: INCORRECT CONNECTION PARAMETERS. Check log file " \
+                        "for detailed error message.\033[1;37;40m"
+            print(error_msg)
+            print(dash_line)
 
-        instruments = [str(firs_instrument)]
+    def disconnect(self):
 
-        instruments_list = list(args_instruments.keys())
+        if logging.getLevelName('DEBUG') > 1:
+            logging.debug(f'ROFEXClient: Disconnecting.')
 
-        for instrument in instruments_list:
-            instruments.append(str(instrument))
+        print(dash_line)
+        message = f'Terminating connection'
+        print(message.center(width))
+        print(dash_line)
+        pyRofex.close_websocket_connection()
+        sys.exit(0)
+
+    def subscribe_instruments(self, args):
 
         entries = [pyRofex.MarketDataEntry.BIDS,
                    pyRofex.MarketDataEntry.OFFERS,
                    pyRofex.MarketDataEntry.LAST]
+        try:
+            for instrument in args:
+                self.subscribed_instruments.append(instrument)
 
-        # TODO cambiar
-        pyRofex.market_data_subscription(tickers=instruments,
-                                         entries=entries)
+                if logging.getLevelName('DEBUG') > 1:
+                    logging.debug(f'ROFEXClient: Subscribing to {instrument} MarketData')
+
+                message = f'Subscribing to {instrument[0]} MarketData'
+                print(message)
+
+                pyRofex.market_data_subscription(tickers=instrument,
+                                                 entries=entries)
+        except Exception as e:
+
+            if logging.getLevelName('DEBUG') > 1:
+                logging.debug(f'ROFEXClient ERROR: En exception occurred {e}')
+            e = str(e).upper()
+            error_msg = f"\033[0;30;47mERROR: {e} Check log file " \
+                        "for detailed error message.\033[1;37;40m"
+            print(error_msg)
+            print(dash_line)
+
+    def process_market_data_message(self, message):
+
+        try:
+            ticker = message.get('instrumentId').get('symbol')
+            ticker_entries = [pyRofex.MarketDataEntry.BIDS, pyRofex.MarketDataEntry.OFFERS]
+            full_book = pyRofex.get_market_data(ticker, ticker_entries, depth=10)
+            bid_book = full_book.get('marketData').get('BI')
+            ask_book = full_book.get('marketData').get('OF')
+            last_trade = [message.get('marketData').get('LA')]
+
+            print(dash_line)
+            print(f"----------- {ticker} - MarketData ".ljust(width, '-'))
+            print("\nBID Book")
+            print(pd.DataFrame().from_dict(bid_book))
+            print("\nASK Book")
+            print(pd.DataFrame().from_dict(ask_book))
+            print("\nLAST Price")
+            print(pd.DataFrame().from_dict(last_trade))
+
+        except Exception:
+            traceback.print_exc()
+            pass
 
 
 def clear_screen():
@@ -67,6 +191,12 @@ def clear_screen():
 
 
 if __name__ == '__main__':
-    rofex_client = ConnectionROFEX()
-    rofex_client.connect()
-    rofex_client.subscribe_instruments("GGALOct20")
+    rofex_client = ROFEXClient("fedejbrun5018", "ugklxY0*", "REM5018", "demo")
+    rofex_client.subscribe_instruments([["GGALOct20"], ["GGALDic20"], ["DODic20"], ["DOOCt20"]])
+
+    while True:
+        try:
+            for _ in range(1):
+                pass
+        except KeyboardInterrupt:
+            rofex_client.disconnect()
