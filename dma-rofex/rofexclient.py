044fedc8b1acdd70c566048e1e3f0440befb4642
Fede Brun GDP <fedejbrun@gmail.com>
2020-10-08 15:51:39 -0300

    Add get_all_orders_status and tidy up Dataframe.

--- dma-rofex/rofexclient.py
+++ dma-rofex/rofexclient.py
@@ -7,6 +7,7 @@ import time
 from pprint import pprint
 import pandas as pd
 from threading import Lock
+import datetime as dt
 
 import pyRofex
 
@@ -31,6 +32,9 @@ class ROFEXClient:
     def __init__(self, user, password, account, environment, verbose=3):
 
         self.subscribed_instruments = []
+        self.user = user
+        self.password = password
+        self.account = account
 
         # Define environment
         env_param = environment.lower()
@@ -38,12 +42,13 @@ class ROFEXClient:
             'demo': pyRofex.Environment.REMARKET,
             'live': pyRofex.Environment.LIVE,
         }
-        env = environments.get(env_param)
+        self.env = environments.get(env_param)
 
         # Create logging file
         # TODO Implementar validacion para Windows/Linux
-        filename = 'c:/logs/ROFEX' + env_param \
-                   + time.strftime('%Y-%m-%d_%H%M%S', time.localtime(time.time())) + '.log'
+        self.session_time = dt.datetime.now()
+        filename = f'c:/logs/ROFEX{env_param}{self.session_time.year}-{self.session_time.month}-' \
+                   f'{self.session_time.hour}_{self.session_time.second}.log'
         logging.basicConfig(level=logging.DEBUG, format='%(asctime)s-%(threadName)s-%(message)s', filename=filename)
 
         # TODO No entiendo muy bien cual es la funcion de los AddLevelName
@@ -60,7 +65,7 @@ class ROFEXClient:
             logging.addLevelName('DEBUG', 30)
 
         try:
-            self.connect(user, password, account, env)
+            self.connect(self.user, self.password, self.account, self.env)
         except KeyboardInterrupt:
             self.disconnect()
 
@@ -113,7 +118,7 @@ class ROFEXClient:
     #   Start UTILITY FUNCTIONS definition
     # ==================================================================================================================
 
-    def connect(self, user, password, account, environment, ):
+    def connect(self, user, password, account, environment ):
         """
         Implements Connection to ROFEX DMA Server.
         (demo = reMarkets; live = ROFEX)
@@ -254,17 +259,37 @@ class ROFEXClient:
             ticker = message.get('instrumentId').get('symbol')
             ticker_entries = [pyRofex.MarketDataEntry.BIDS, pyRofex.MarketDataEntry.OFFERS]
             full_book = pyRofex.get_market_data(ticker, ticker_entries, depth=10)
-            bid_book = full_book.get('marketData').get('BI')
-            ask_book = full_book.get('marketData').get('OF')
-            last_trade = [message.get('marketData').get('LA')]
 
-            full_book_df = pd.concat([pd.DataFrame().from_dict(bid_book), pd.DataFrame().from_dict(ask_book)], axis=1)
+            if len(full_book.get('marketData').get('BI')) !=0\
+                    and len(full_book.get('marketData').get('OF')) != 0:
+                bid_book = full_book.get('marketData').get('BI')
+                bid_book_df = pd.DataFrame().from_dict(bid_book)
+                bid_book_df.columns = ['BP', 'BS']
 
-            lk.acquire()
-            print(dash_line)
-            print(f"----------- {ticker} - MarketData ".ljust(width, '-'))
-            print(full_book_df)
-            lk.release()
+                ask_book = full_book.get('marketData').get('OF')
+                ask_book_df = pd.DataFrame().from_dict(ask_book)
+                ask_book_df.columns = ['OP', 'OS']
+
+                last_trade = [message.get('marketData').get('LA')]
+
+                full_book_df = pd.concat([bid_book_df, ask_book_df], axis=1)
+                full_book_df = full_book_df[['BS', 'BP', 'OP', 'OS']]
+                full_book_df.fillna(0, inplace=True)
+                lk.acquire()
+                print(dash_line)
+                print(f"----------- {ticker} - MarketData ".ljust(width, '-'))
+                print("   BID         ASK")
+                print(full_book_df)
+                lk.release()
+
+            else:
+                empty_MD = pd.DataFrame.from_dict({'BS': [0], 'BP': [0],  'OP': [0], 'OS': [0]})
+                lk.acquire()
+                print(dash_line)
+                print(f"----------- {ticker} - MarketData ".ljust(width, '-'))
+                print("   BID              ASK")
+                print(empty_MD)
+                lk.release()
 
         except Exception:
             traceback.print_exc()
@@ -292,7 +317,7 @@ class ROFEXClient:
                                        order_type=pyRofex.OrderType.LIMIT)
             lk.acquire()
             print(dash_line)
-            print("Send Order Response:")
+            print(f"----------- Send Order Response: ".ljust(width, '-'))
             pprint(order)
             active_orders.append(order)
             lk.release()
@@ -300,7 +325,7 @@ class ROFEXClient:
             order_status = pyRofex.get_order_status(order["order"]["clientId"])
             lk.acquire()
             print(dash_line)
-            print("Order Status Response:")
+            print(f"----------- Order Status Response: ".ljust(width, '-'))
             pprint(order_status)
             lk.release()
 
@@ -324,14 +349,14 @@ class ROFEXClient:
 
             lk.acquire()
             print(dash_line)
-            print("Cancel Order Response:")
+            print(f"----------- Cancel Order Response: ".ljust(width, '-'))
             pprint(cancel_order)
             lk.release()
 
             order_status = pyRofex.get_order_status(ClOrdId)
             lk.acquire()
             print(dash_line)
-            print("Cancel Order Status Response:")
+            print(f"----------- Cancel Order Status Response: ".ljust(width, '-'))
             pprint(order_status)
             lk.release()
 
@@ -349,7 +374,38 @@ class ROFEXClient:
 
     def get_all_order_status(self):
         # TODO implement
-        pass
+        try:
+            all_orders_status = pyRofex.get_all_orders_status(self.account, self.env)
+            session_orders = []
+
+            for order in all_orders_status.get('orders'):
+                # TODO filtrar por sesion
+                parse_time = order.get('transactTime').split('.')[0]
+                ord_time = dt.datetime(
+                    int(parse_time[:4]),
+                    int(parse_time[4:6]),
+                    int(parse_time[6:8]),
+                    int(parse_time[9:11]),
+                    int(parse_time[12:14]),
+                    int(parse_time[15:17])
+                )
+
+                if ord_time > self.session_time:
+                    session_orders.append(order)
+
+            lk.acquire()
+            print(dash_line)
+            print(f"----------- All Orders Status: ".ljust(width, '-'))
+            pprint(session_orders)
+            lk.release()
+
+        except Exception as e:
+            if logging.getLevelName('DEBUG') > 1:
+                logging.debug(f'ROFEXClient ERROR: En exception occurred {e}')
+
+            error_msg = "\033[0;30;47mERROR: Check log file for detailed error message.\033[1;37;40m"
+            print(error_msg)
+            print(dash_line)
 
     def get_market_data(self):
         # TODO implement
@@ -428,15 +484,23 @@ if __name__ == '__main__':
     quoting_price = rofex_client.get_quoting_price(price)
     quantity = rofex_client.get_market_qty("GGALOct20")
     rofex_client.placer_order("GGALOct20", "buy", price, 1)
+    price = rofex_client.get_market_price("GGALOct20")
+    rofex_client.placer_order("GGALOct20", "buy", price + 0.5, 1)
+    price = rofex_client.get_market_price("GGALOct20")
+    rofex_client.placer_order("GGALOct20", "buy", price + 0.5, 1)
 
-    time.sleep(4)
+    rofex_client.get_all_order_status()
+
+    time.sleep(6)
     lk.acquire()
     print(dash_line)
-    print("CANCEL ORDER")
+    print("CANCEL ORDERS")
     lk.release()
 
-    cancel_orderId = active_orders.pop().get('order').get('clientId')
-    rofex_client.cancel_order(cancel_orderId)
+    for order in active_orders:
+        time.sleep(3)
+        cancel_orderId = order.get('order').get('clientId')
+        rofex_client.cancel_order(cancel_orderId)
 
     while True:
         try:
